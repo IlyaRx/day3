47b879ce250e1905ed2fd9ee77f74545c445e5a2
student11 <57040550+IlyaRx@users.noreply.github.com>
2023-05-23 10:19:25 +0300

    выполнение задания

--- day3/Employee.cs
+++ day3/Employee.cs
@@ -8,5 +8,54 @@ namespace day3
 {
     class Employee
     {
+        public Employee(string lastNamek, string firstNamek, string surnamek, int departmentk, int salaryk)
+        {
+            idAuto++;
+            id = idAuto;
+            lastName = lastNamek;
+            firstName = firstNamek;
+            surname = surnamek;
+            department = departmentk;
+            salary = salaryk;
+        }
+
+        private static int idAuto = 0; // счётчик
+        private int id; // ИД сотрудника 
+        private string lastName; // фамилия
+        private string firstName; // Имя
+        private string surname; // отчество
+        private int department; // отдел от 1 до 5
+        private int salary; // зарплата
+        
+        public int ID
+        {
+            get{ return id; }
+        }
+        public string LastName // фамилия
+        {
+            get{ return lastName; }
+            set{ lastName = value; }
+        }
+        public string FirstName // Имя
+        {
+            get { return firstName; }
+            set { firstName = value; }
+        }
+        public string Surname // отчество
+        {
+            get{ return surname; }
+            set{ surname = value; }
+        }
+        public int Department // отдел от 1 до 5
+        {
+            get{ return department; }
+            set{ department = value; }
+        }
+        public int Salary // зарплата
+        {
+            get{ return salary; }
+            set{ salary = value; }
+        }
+
     }
 }
--- day3/Program.cs
+++ day3/Program.cs
@@ -8,8 +8,131 @@ namespace day3
 {
     class Program
     {
+        static void average(List<Employee> employees)
+        {
+            Console.WriteLine($"{SumSalary(employees)/employees.Count}");
+        }
+        static void sort(List<Employee> employees)
+        {
+            Employee MinSalary;
+            for (int i = 0; i < employees.Count; i++)
+            {
+                for (int j = i + 1; j < employees.Count; j++)
+                {
+                    if (employees[i].ID > employees[j].ID)
+                    {
+                        MinSalary = employees[i];
+                        employees[i] = employees[j];
+                        employees[j] = MinSalary;
+                    }
+                }
+            }
+        }
+        static void ListPeople(List<Employee> employees)
+        {
+            foreach(var e in employees)
+            {
+                Console.WriteLine($"\n\nid = {e.ID}" +
+                    $"\n{e.LastName}" +
+                    $"\n{e.FirstName}" +
+                    $"\n{e.Surname}" +
+                    $"\n{e.Department}" +
+                    $"\n{e.Salary}");
+            }
+        }
+
+        static int SumSalary(List<Employee> employees)
+        {
+            int sum = 0;
+            foreach(var e in employees)
+            {
+                sum += e.Salary;
+            }
+            Console.WriteLine($"Сумма затрат на зарплату = {sum}");
+            return sum;
+        }
+
+        static void MinSalary(List<Employee> employees)
+        {
+            Employee MinSalary;
+            for (int i = 0; i < employees.Count; i++)
+            {
+                for (int j = i + 1; j < employees.Count; j++)
+                {
+                    if (employees[i].Salary > employees[j].Salary)
+                    {
+                        MinSalary = employees[i];
+                        employees[i] = employees[j];
+                        employees[j] = MinSalary;
+                    }
+                }
+            }
+            Console.WriteLine($"\n\nid = {employees[0].ID}" +
+                $"\n{employees[0].LastName}" +
+                $"\n{employees[0].FirstName}" +
+                $"\n{employees[0].Surname}" +
+                $"\n{employees[0].Department}" +
+                $"\n{employees[0].Salary}");
+            sort(employees);
+        }
+
+        static void MaxSalary(List<Employee> employees)
+        {
+            Employee MaxSalary;
+            for (int i = 0; i < employees.Count; i++)
+            {
+                for (int j = i + 1; j < employees.Count; j++)
+                {
+                    if (employees[i].Salary > employees[j].Salary)
+                    {
+                        MaxSalary = employees[i];
+                        employees[i] = employees[j];
+                        employees[j] = MaxSalary;
+                    }
+                }
+            }
+            Console.WriteLine($"\n\nid = {employees[employees.Count-1].ID}" +
+                              $"\n{employees[employees.Count - 1].LastName}" +
+                              $"\n{employees[employees.Count - 1].FirstName}" +
+                              $"\n{employees[employees.Count - 1].Surname}" +
+                              $"\n{employees[employees.Count - 1].Department}" +
+                              $"\n{employees[employees.Count - 1].Salary}");
+            sort(employees);
+        }
+        static void FIO(List<Employee> employees)
+        {
+            foreach (var e in employees)
+            {
+                Console.WriteLine($"\n{e.LastName}" +
+                    $"\n{e.FirstName}" +
+                    $"\n{e.Surname}");
+            }
+        }
         static void Main(string[] args)
         {
+            List<Employee> people = new List<Employee>()
+            {
+            new Employee("Фамилия1", "Имя", "отчество", 1,10001),
+            new Employee("Фамилия2", "Имя", "отчество", 3,100023),
+            new Employee("Фамилия3", "Имя", "отчество", 2,1035400),
+            new Employee("Фамилия4", "Имя", "отчество", 3,13000),
+            new Employee("Фамилия5", "Имя", "отчество", 1,1000),
+            };
+
+            ListPeople(people);
+            Console.WriteLine("//////////////////////////////////////////////////////////");
+            SumSalary(people);
+            Console.WriteLine("//////////////////////////////////////////////////////////");
+            MinSalary(people);
+            Console.WriteLine("//////////////////////////////////////////////////////////");
+            MaxSalary(people);
+            Console.WriteLine("//////////////////////////////////////////////////////////");
+            average(people);
+            Console.WriteLine("//////////////////////////////////////////////////////////");
+            FIO(people);
+
+            Console.ReadKey();
+
         }
     }
 }
